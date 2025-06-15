997fa7f29638cc724ad88077d37c68992302fae4
Jibby <Jibbajabbafic@users.noreply.github.com>
2025-06-15 23:50:32 +0100

    fix(main): run all bot loading in the same async process (#34)
    * fix(main): run all bot loading in the same async process
    
    * fix: remove comments
    
    * fix: stop discord log spam
    
    * make thinking message ephemeral
    
    * fix(test): make main tests pass again
    
    * fix(test): fix yt unit tests
    
    ---------
    
    Co-authored-by: Jim Wild <wild.james343@gmail.com>

--- src/balaambot/main.py
+++ src/balaambot/main.py
@@ -9,21 +9,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Discord provides a nice default coloured log format
+discord.utils.setup_logging(level=logging.DEBUG)
+# Discord gets very spammy on DEBUG
+logging.getLogger("discord").setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
 bot = commands.Bot(
-    command_prefix="!",
+    command_prefix=commands.when_mentioned_or("!"),
     case_insensitive=True,
     intents=intents,
     description="A bot for the NaughtyBoys",
 )
-
-BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 
 @bot.event
@@ -49,21 +52,27 @@ async def load_extensions() -> None:
             await bot.load_extension(ext_path)
 
 
-def start() -> None:
-    """Entrypoint for the bot; schedules extension loading and runs the bot."""
-    if BOT_TOKEN is None or BOT_TOKEN == "":
+async def main() -> None:
+    """Main async process that runs the bot."""
+    # Check the token is valid
+    if DISCORD_BOT_TOKEN is None or DISCORD_BOT_TOKEN == "":
         msg = "DISCORD_BOT_TOKEN environment variable is not set."
         raise ValueError(msg)
-    if '"' in BOT_TOKEN:
+    if '"' in DISCORD_BOT_TOKEN:
         msg = (
             "DISCORD_BOT_TOKEN contains invalid characters. "
             'Do not wrap the token in "..." inside the .env file!'
         )
         raise ValueError(msg)
-    asyncio.run(load_extensions())
     logger.info("Starting bot...")
-    bot.run(BOT_TOKEN)
+    async with bot:
+        # Loads all files in bot_commands
+        await load_extensions()
+        # Start the bot
+        await bot.start(DISCORD_BOT_TOKEN)
 
 
-if __name__ == "__main__":
-    start()
+def start() -> None:
+    """Entrypoint for the bot. Starts the main async process."""
+    # Bot must be run this way so all tasks run in the same process
+    asyncio.run(main())
