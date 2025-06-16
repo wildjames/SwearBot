cbb4ac643cced2a39339d3a76e2acb7c7e70c6b7
Jim Wild <wild.james343@gmail.com>
2025-06-16 10:21:27 +0100

    Fix typos, comment, import, and expand utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,16 @@
-def test_sec_to_string():
-    from src.balaambot.utils import sec_to_string
-    assert sec_to_string(5) == "00:05"
-    assert sec_to_string(65) == "01:05"
-    assert sec_to_string(3665) == "01:01:05"
+import pytest
+from balaambot.utils import sec_to_string
+
+@pytest.mark.parametrize(
+    "seconds,expected",
+    [
+        (0, "00:00"),
+        (5, "00:05"),
+        (65, "01:05"),
+        (3600, "01:00:00"),
+        (3665, "01:01:05"),
+    ],
+)
+def test_sec_to_string(seconds, expected):
+    assert sec_to_string(seconds) == expected
+
