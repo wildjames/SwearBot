546ef6809d80c8b37eeb8e41a21d897518c2a378
James Wild <wild.james343@gmail.com>
2025-06-20 19:23:13 +0000

    Add a test for grabbing age restricted content in the demo file

--- tests/youtube/demo_youtube_audio.py
+++ tests/youtube/demo_youtube_audio.py
@@ -16,57 +16,77 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
+async def fetch_audio(
+    test_url: str,
+    username: str | None = None,
+    password: str | None = None,
+) -> None:
+    logger.info("Testing audio fetching for URL: %s", test_url)
+    # Fetch and cache audio
+    t0 = asyncio.get_event_loop().time()
+    cache_path = await fetch_audio_pcm(test_url, username=username, password=password)
+    t1 = asyncio.get_event_loop().time()
+    logger.info("Fetched audio cache at: %s", cache_path)
+    logger.info("Fetch took %.2f seconds", t1 - t0)
+
+async def test_get_metadata(test_url: str) -> None:
+    # Get track name
+    t0 = asyncio.get_event_loop().time()
+    track_metadata = await get_youtube_track_metadata(test_url)
+    t1 = asyncio.get_event_loop().time()
+    if track_metadata:
+        logger.info("Track name: %s", track_metadata["title"])
+        logger.info("Track runtime: %s seconds", track_metadata["runtime"])
+        logger.info("Track name fetch took %.2f seconds", t1 - t0)
+    else:
+        logger.warning("Could not fetch track name for URL: %s", test_url)
+
+async def load_cached_audio_pcm(test_url: str) -> None:
+    # Read raw PCM bytes
+    t0 = asyncio.get_event_loop().time()
+    pcm_data = get_audio_pcm(test_url)
+    t1 = asyncio.get_event_loop().time()
+    if pcm_data:
+        logger.info("Loaded %s bytes of PCM data", len(pcm_data))
+        logger.info("PCM data read took %.2f seconds", t1 - t0)
+
+    await asyncio.sleep(5)
+
+    # Remove cached file
+    removed = remove_audio_pcm(test_url)
+    logger.info("Cache removed: %s", removed)
+
+async def get_playlist(playlist_url: str) -> None:
+    logger.info("Fetching a playlist of URLs: %s", playlist_url)
+    urls = await get_playlist_video_urls(playlist_url)
+    logger.info("URLs:")
+    for url in urls:
+        logger.info(" - %s", url)
+
+async def search(search: str) -> None:
+    # search for a video
+    logger.info("Fetching search results for query '%s'", search)
+    results = await search_youtube(search)
+    logger.info("Results:")
+    for result in results:
+        logger.info(" - %s", result)
+
+
 if __name__ == "__main__":
-    test_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
     async def main() -> None:
         """Test the audio fetching and caching functionality."""
-        logger.info("Testing audio fetching for URL: %s", test_url)
-        # Fetch and cache audio
-        t0 = asyncio.get_event_loop().time()
-        cache_path = await fetch_audio_pcm(test_url)
-        t1 = asyncio.get_event_loop().time()
-        logger.info("Fetched audio cache at: %s", cache_path)
-        logger.info("Fetch took %.2f seconds", t1 - t0)
+        test_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        test_playlist_url = "https://www.youtube.com/watch?v=Z0Uh3OJCx3o&list=PLJDafirWnxGR5H0rSeJKgxC6rIj78JKce"
+        test_search_query = "Rick Astley Never Gonna Give You Up"
 
-        # Get track name
-        t0 = asyncio.get_event_loop().time()
-        track_metadata = await get_youtube_track_metadata(test_url)
-        t1 = asyncio.get_event_loop().time()
-        if track_metadata:
-            logger.info("Track name: %s", track_metadata["title"])
-            logger.info("Track runtime: %s seconds", track_metadata["runtime"])
-            logger.info("Track name fetch took %.2f seconds", t1 - t0)
-        else:
-            logger.warning("Could not fetch track name for URL: %s", test_url)
+        await fetch_audio(test_url)
+        await test_get_metadata(test_url)
+        await load_cached_audio_pcm(test_url)
+        await get_playlist(test_playlist_url)
+        await search(test_search_query)
 
-        # Read raw PCM bytes
-        t0 = asyncio.get_event_loop().time()
-        pcm_data = get_audio_pcm(test_url)
-        t1 = asyncio.get_event_loop().time()
-        if pcm_data:
-            logger.info("Loaded %s bytes of PCM data", len(pcm_data))
-            logger.info("PCM data read took %.2f seconds", t1 - t0)
-
-        await asyncio.sleep(5)
-
-        # Remove cached file
-        removed = remove_audio_pcm(test_url)
-        logger.info("Cache removed: %s", removed)
-
-        playlist_url = "https://www.youtube.com/watch?v=Z0Uh3OJCx3o&list=PLJDafirWnxGR5H0rSeJKgxC6rIj78JKce"
-        logger.info("Fetching a playlist of URLs: %s", playlist_url)
-        urls = await get_playlist_video_urls(playlist_url)
-        logger.info("URLs:")
-        for url in urls:
-            logger.info(" - %s", url)
-
-        # search for a video
-        search = "aphex twin"
-        logger.info("Fetching search results for query '%s'", search)
-        results = await search_youtube(search)
-        logger.info("Results:")
-        for result in results:
-            logger.info(" - %s", result)
+        age_restricted_url = "https://www.youtube.com/watch?v=wiRRsHPTSC8"
+        await fetch_audio(age_restricted_url)
 
     asyncio.run(main())
